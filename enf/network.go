3698c4506860d77ff1b1333d7bbd9425df010b3b
Dylan Irlbeck <irlbeck2@illinois.edu>
2019-09-03 13:10:04 -0500

    Added NetworkRequest and Network structs

--- enf/network.go
+++ enf/network.go
@@ -1,3 +1,15 @@
 package enf
 
 type NetworkService service
+
+type NetworkRequest struct {
+	Name        *string `json:"name"`
+	Description *string `json:"description"`
+}
+
+type Network struct {
+	Name        *string `json:"name"`
+	Network     *string `json:"network"`
+	Description *string `json:"description"`
+	Status      *string `json:"status"`
+}
