b5a11fd73289885be95574bf6bf353046e03adb6
Dylan Irlbeck <irlbeck2@illinois.edu>
2019-09-09 15:16:05 -0500

    Adding CreateNetwork method with a unit test

--- enf/network_test.go
+++ enf/network_test.go
@@ -2,6 +2,7 @@ package enf
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -92,6 +93,62 @@ func TestNetworkService_GetNetwork(t *testing.T) {
 			`)
 	})
 	network, _, err := client.Network.GetNetwork(context.Background(), "N/n")
+	if err != nil {
+		t.Errorf("Network.GetNetwork returned error: %v", err)
+	}
+
+	want := &Network{
+		Name:        String("TestNetwork 1"),
+		Network:     String("N/n"),
+		Description: String("This is a network."),
+		Status:      String("ACTIVE"),
+	}
+	if !reflect.DeepEqual(network, want) {
+		t.Errorf("Network.GetNetwork returned %+v, want %+v", network, want)
+	}
+}
+
+func TestNetworkService_CreateNetwork(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	input := &NetworkRequest{
+		Name:        String("TestNetwork 1"),
+		Description: String("This is a new network."),
+	}
+
+	wantAcceptHeaders := []string{mediaTypeJson}
+	wantContentTypeHeaders := []string{mediaTypeJson}
+	mux.HandleFunc("/api/xcr/v2/domains/1/nws", func(w http.ResponseWriter, r *http.Request) {
+		v := new(NetworkRequest)
+		_ = json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "POST")
+		testHeader(t, r, "Accept", strings.Join(wantAcceptHeaders, ", "))
+		testHeader(t, r, "Content-Type", strings.Join(wantContentTypeHeaders, ", "))
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+
+		fmt.Fprint(w, `{
+			"data": [
+			  {
+				"name": "TestNetwork 1",
+				"network": "N/n",
+				"description": "This is a network.",
+				"status": "ACTIVE"
+				}
+			],
+			"page": {
+			  "curr": -1,
+			  "next": -1,
+			  "prev": -1
+			}
+		  }
+			`)
+	})
+
+	network, _, err := client.Network.CreateNetwork(context.Background(), "1", input)
 	if err != nil {
 		t.Errorf("Network.GetNetwork returned error: %v", err)
 	}
