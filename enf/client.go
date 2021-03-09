783561d4898fbe2f03b7a9359e45f8c9b6453d11
Venkatakumar Srinivasan <venkat@xaptum.com>
2021-03-08 20:57:02 -0600

    copyright notices

--- enf/client.go
+++ enf/client.go
@@ -1,3 +1,23 @@
+//-------------------------------------------------------------------------------------------
+//
+// XAPTUM CONFIDENTIAL
+// __________________
+//
+//  2021(C) Xaptum, Inc.
+//  All Rights Reserved.Patents Pending.
+//
+// NOTICE:  All information contained herein is, and remains
+// the property of Xaptum, Inc.  The intellectual and technical concepts contained
+// herein are proprietary to Xaptum, Inc and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+// Dissemination of this information or reproduction of this material
+// is strictly forbidden unless prior written permission is obtained
+// from Xaptum, Inc.
+//
+// @author Venkatakumar Srinivasan
+// @since March 08, 2021
+//
+//-------------------------------------------------------------------------------------------
 package enf
 
 import (
